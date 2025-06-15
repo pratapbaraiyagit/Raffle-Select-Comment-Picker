45e53edb2a65209555850a0871af1ac4d355b145
pratapbaraiyagit <121426664+pratapbaraiyagit@users.noreply.github.com>
2025-06-15 17:09:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import { App } from './app';
+import { HelmetProvider } from 'react-helmet-async';
+import ReactDOM from 'react-dom/client';
+import 'sanitize.css/sanitize.css';
+import store from './Store/Index';
+import { Provider } from 'react-redux';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <HelmetProvider>
+    <Provider store={store}>
+      <App />
+      <ToastContainer position="bottom-center" />
+    </Provider>
+  </HelmetProvider>,
+);
 
